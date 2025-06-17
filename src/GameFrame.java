b287c7e701d9cbfdbc007a8f14a5f3f0f77327ea
tamoyou1230 <tamoyou1230@gmail.com>
2025-06-17 11:43:58 +0900

    第１３章

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,19 +6,47 @@ public class GameFrame extends MyFrame{
 		
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
+		GameWorld.stage=1;
+		GameWorld.score=0;
+		
+		while(true) {
+			
+		GameWorld.player.x=100;
+		GameWorld.player.y=300;
+		
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
 		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		GameWorld.enemies.add(new EnemyBase(100,50,GameWorld.stage,0));
+		GameWorld.enterPressed=false;
 		
 		while(true) {
 			
 			clear();
+			drawString("Stage = "+GameWorld.stage,300,50,15);
+			drawString("Score = "+GameWorld.score,300,80,15);
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+			
+			if(GameWorld.enemies.size()==0) {
+				setColor(0,0,0);
+				drawString("クリア！",100,200,40);
+				if(GameWorld.enterPressed) {
+					GameWorld.stage++;
+					break;
+				}
+			}else if(GameWorld.player.y<0){
+				setColor(0,0,0);
+				drawString("ゲームオーバー！",50,200,40);
+				if(GameWorld.enterPressed) {
+					GameWorld.stage=1;
+					GameWorld.score=0;
+					break;
+				}
+			}
 			
 			for(int i=0 ; i<GameWorld.enemies.size(); i++) {
 				Enemy e=GameWorld.enemies.get(i);
@@ -31,9 +59,11 @@ public class GameFrame extends MyFrame{
 					}
 					
 				}
+				
 			}
 			
 			sleep(0.03);
+		}
 		}
 	}
 		
@@ -43,6 +73,16 @@ public class GameFrame extends MyFrame{
 			Enemy e=GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+		
+		int i=0;
+		while(i<GameWorld.enemies.size()) {
+			Enemy e=GameWorld.enemies.get(i);
+			if(e.y>400) {
+				GameWorld.enemies.remove(i);
+			}else {
+				i++;
+			}
 		}
 		
 	}
@@ -86,12 +126,13 @@ public class GameFrame extends MyFrame{
 				Enemy e=GameWorld.enemies.get(j);
 				
 					if(checkHit(e,b)) {
-					System.out.println("あたり");
-					hits++;
-					e.life--;
+						System.out.println("あたり");
+						hits++;
+						e.life--;
 					}
 					if(e.life<=0) {
-					GameWorld.enemies.remove(j);
+						GameWorld.score+=e.score;
+						GameWorld.enemies.remove(j);
 					}
 					else {
 					j++;
